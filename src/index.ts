54a19ee8a325e85f3bd17a06e30a0668a84fb0e0
Ho Nyeong <dev.honing@gmail.com>
2023-11-15 12:28:01 +0900

    TypeScript의 실행법 정리

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 // index.ts
 
-// 콘솔 출력법 === 자바스크립트 작성법
+// 기본 출력법 === 자바스크립트 작성법
+// .ts 출력
 console.log("Hello, TypeScript");
+
+// .ts 실행
+//* 1. 디렉토리를 지정하고 실행 - node dist/index.js
+//* 2. 디렉토리로 이동하고 실행 - cd .. / cd 등으로 디렉토리 이동하고 node index.js
+//* 3. Node.js 디버거 세팅
+//* 4. package.json을 수정 - npm start
